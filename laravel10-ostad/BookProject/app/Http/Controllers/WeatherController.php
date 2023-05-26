d023e7a882df13226be7327a4c2e6a8fcb190385
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-27 00:08:18 +0600

    optional parameter

--- laravel10-ostad/BookProject/app/Http/Controllers/WeatherController.php
+++ laravel10-ostad/BookProject/app/Http/Controllers/WeatherController.php
@@ -9,8 +9,8 @@ class WeatherController extends Controller
     // function getWeather($city){ // route এ যে কয়টা parameter ধরবেন, funciton এ ঠিক সেই কয়টা argument pass করতে হবে
     //     return "weather in {$city} is good";
     // }
-    
-    function getWeather($city = null){
+
+    function getWeather($city = 'ABCD'){
         return "weather in {$city} is good";
     }
 }
