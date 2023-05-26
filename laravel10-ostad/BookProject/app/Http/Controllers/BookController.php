46db1092af2330ec2049e04f57a53c822a196fb3
dulalsheikh5205 <dulalsheikh5205@gmail.com>
2023-05-26 21:11:25 +0600

    create books array variable

---
+++ laravel10-ostad/BookProject/app/Http/Controllers/BookController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    private $books = [
+        [
+            'author'=>'Jane Austen',
+            'title'=> 'Pride and Prejudice'
+        ],
+        [
+            'author'=>'F. Scott Fitzgerald',
+            'title'=>'The Great Gatsby'
+        ],
+        [
+            'author'=> 'George Orwell',
+            'title'=> '1984'
+        ],
+        [
+            'author'=>'J.R.R. Tolkien',
+            'title'=> 'The Lord of the Rings'
+        ],
+        [
+            'author'=>'Harper Lee',
+            'title'=>'To kill a Mockingbird'
+        ]
+    ];
+}
